1bc3ebd3ced745e05614e37b5d01c110a0838ed7
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-10-21 11:56:59 +0900

    Loop #1

--- src/main/java/chapter01/Ex16Switch.java
+++ src/main/java/chapter01/Ex16Switch.java
@@ -6,26 +6,31 @@ public class Ex16Switch {
 		char grade = ' ';
 		int score = 76;
 		
-	switch(score / 10) {
-		case 10:
-		case 9 : {
-			grade = 'A';
-			break;
-		} case 8: {
-			grade = 'B';
-			break;
-		} case 7: {
-			grade = 'C';
-			break;
-		} case 6: {
-			grade = 'D';
-			break;
-		} default: {
-			grade = 'F';
-			break;
+		switch(score / 10) {
+			case 10:	// score = 100
+			case 9: {	// 90 <= score <= 99  
+				grade = 'A';
+				break;
+			}
+			case 8: {	// 80 <= score <= 89 
+				grade = 'B';
+				break;
+			}
+			case 7: {	// 70 <= score <= 79
+				grade = 'C';
+				break;
+			}
+			case 60: {	// 60 <= score <= 69
+				grade = 'D';
+				break;
+			}
+			default: {	// score <= 59
+				grade = 'F';
+				break;
+			}			
 		}
-	
+		
+		System.out.println("학점은 " + grade + "입니다.");		
+	}
 
-		}	
-	}	
 }
--- src/main/java/chapter01/Ex24Gugudan.java
+++ src/main/java/chapter01/Ex24Gugudan.java
@@ -3,10 +3,10 @@ package chapter01;
 public class Ex24Gugudan {
 
 	public static void main(String[] args) {
-		for(int j = 1; j<=9; j++) {
+		for(int i = 1; i<=9; i++) {
 
-			for(int i = 1; i <= 9; i++) {
-				System.out.println(j + " X " + i + " = " + j*i);
+			for(int j = 1; j <= 9; j++) {
+				System.out.println(i + " X " + j + " = " + i*j);
 			}
 
 			System.out.println("================");
