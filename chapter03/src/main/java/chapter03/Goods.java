6de2ebfa12e747d0eb393133549302fe1e17fe94
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-10-25 14:02:58 +0900

    객체지향프로그래밍 #2

--- chapter03/src/main/java/chapter03/Goods.java
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -1,11 +1,70 @@
 package chapter03;
 
 public class Goods {
-	String name;
-	int price;
-	int countStock;
-	int countSold;
+	private String name;
+	private int price;
+	private int countStock;
+	private int countSold;
 	
+	
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+
+
+	public int getCountStock() {
+		return countStock;
+	}
+
+
+
+
+	public void setCountStock(int countStock) {
+		this.countStock = countStock;
+	}
+
+
+
+
+	public int getCountSold() {
+		return countSold;
+	}
+
+
+
+
+	public void setCountSold(int countSold) {
+		this.countSold = countSold;
+	}
+
+
+
+
 	// source - generate to string 해야 GoodsApp에서 출력 가능 / 하지 않으면 해시 값이 출력된다
 	@Override
 	public String toString() {
