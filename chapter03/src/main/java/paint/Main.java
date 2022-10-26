5b1394e568cc3d862499ca8c15037736c341dcde
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-10-26 15:26:14 +0900

    다형성 - 오버라이딩

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -14,19 +14,31 @@ public class Main {
 		pt1.show(false);
 		
 		// 기본 생성자
-		ColorPoint pt2 = new ColorPoint();
+//		ColorPoint pt2 = new ColorPoint();
+//		pt2.setX(100); // 코드 재사용
+//		pt2.setY(200);
+//		pt2.setColor("red");
+		
+		// 객체를 만드는 코드 (수정x)
+		Point pt2 = new ColorPoint();
 		pt2.setX(100); // 코드 재사용
 		pt2.setY(200);
-		pt2.setColor("red");
+		// 다운 케스팅 >> colorpoint가 아닌 point(부모) 참조 / 주차장 
+		((ColorPoint)pt2).setColor("red");
 		
-		drawColorPoint(pt2);
+		// 다운 케스팅 수정없이 기본 것 쓸 수 있다
+//		drawColorPoint(pt2);
+		drawPoint(pt2);
+		pt2.show(false);
+		// point에서 if문에서 오바라이드 되어있기 때문에 안해도된다
+//		pt2.show(true);
 	}
 	
 	public static void drawPoint(Point pt) {
 		pt.show();
 	}
-	
-	public static void drawColorPoint(ColorPoint pt) {
-		pt.show();
-	}
+	// 다운케스팅
+//	public static void drawColorPoint(ColorPoint pt) {
+//		pt.show();
+//	}
 }
