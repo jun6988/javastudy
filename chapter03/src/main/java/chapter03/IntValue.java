bb9c4965b906e59181c1e58502bb256c8db65120
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-10-26 13:26:03 +0900

    swap

---
+++ chapter03/src/main/java/chapter03/IntValue.java
@@ -0,0 +1,8 @@
+package chapter03;
+// call by value reference
+public class IntValue {
+	public int value; // private
+	public IntValue(int value) {
+		this.value = value; // 값이 있어야 생성 가능 (public int value;)
+	}
+}
