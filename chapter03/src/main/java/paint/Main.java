9b2cabd90f9233a99de3ef40bc387261d654a935
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-10-26 13:48:37 +0900

    다형성 - 오버로딩

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -4,11 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Point pt = new Point();
-		pt.setX(10);
-		pt.setY(50);
+		Point pt = new Point(10, 50); // point 클래스에서 기본 생성자 생성 하여 오류 나서 밑에 주석하고 값 넣어줌
+//		pt.setX(10);
+//		pt.setY(50);
 		
 		drawPoint(pt);
+		//pt.disappear();
+		// 오버로딩
+		pt.show(false);
 	}
 	
 	public static void drawPoint(Point pt) {
--- chapter03/src/main/java/paint/Point.java
+++ chapter03/src/main/java/paint/Point.java
@@ -3,6 +3,12 @@ package paint;
 public class Point {
 	private int x;
 	private int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
 	public int getX() {
 		return x;
 	}
@@ -15,7 +21,19 @@ public class Point {
 	public void setY(int y) {
 		this.y = y;
 	}
-	public void show() {
+	// 오버로딩 진짜로 하는 것 (시그너쳐 다르게 하여)
+	public void show(boolean visible) {
+		if(visible) {
+			//System.out.println("점(x=" + x + ", y=" + y + ")를 그렸습니다.");
+			show(); // 있는 코드 활용
+		} else {
+			System.out.println("점(x=" + x + ", y=" + y + ")를 지웠습니다.");
+		}
+	}
+	public void show() { // 위 void show가 같아야한다
 		System.out.println("점(x=" + x + ", y=" + y + ")를 그렸습니다.");
 	}
+//	public void disappear() {
+//		System.out.println("점(x=" + x + ", y=" + y + ")를 지웠습니다.");
+//	}
 }
