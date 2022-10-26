df9ae706f651addbd7913fb25e857105aa94a128
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-10-26 17:50:28 +0900

    practice01

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,13 +3,31 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
-//		for(int i = 1; i < 100; i++) {
-//		
-//		}
-		
-		int i = 36;
-		int count = 0;
-		
-		String number = String.valueOf(i); // char at 활용
+
+		for (int i = 1; i < 100; i++) {
+			String number = String.valueOf(i);
+			int count = 0;
+
+			for (int j = 0; j < number.length(); j++) {
+				char c = number.charAt(j);
+				if (c == '3' || c == '6' || c == '9') { // 369 중에 하나라도 포함되어 있다면 박수쳐라
+
+					count++;
+
+				}
+
+			}
+
+			if (count == 1) {
+				System.out.print(number + "	");
+				System.out.println("짝");
+			} else if (count == 2) {
+				System.out.print(number + "	");
+				System.out.println("짝짝");
+
+			}
+
+		}
+
 	}
 }
