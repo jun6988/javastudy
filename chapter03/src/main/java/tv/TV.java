e6cb757c000ebf58985c2f16c23d7ad00484c9e3
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-11-16 17:27:17 +0900

    chapter03

--- chapter03/src/main/java/tv/TV.java
+++ chapter03/src/main/java/tv/TV.java
@@ -1,17 +1,66 @@
-package tv; // test하려고 메인 만드는 것
+package tv;
 
 public class TV {
-	private int channel; // 1~255 사이 값을 가지게 하고 max, min 넘어 갈 시 라운딩 시킬 것
-	private int volume;	 // 0~100 사이 값을 가지게 하고 max, min 넘어 갈 시 라운딩 시킬 것
+	private int channel;    // 1~255 
+	private int volume;     // 0~100
 	private boolean power;
+	
+	public TV(int channel, int volume, boolean power) {
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
+	}
+	
+	public void power(boolean on) {
+		power = on;
+	}
+	
+	public void channel(boolean up) {
+		// channel(channel + (up ? 1 : -1));
+		if(up) {
+			channel(channel + 1);
+		} else {
+			channel(channel - 1);
+		}
+	}
+
+	public void channel(int channel) {
+		if(!power) {
+			return;
+		}
+		
+		if(channel < 1) {
+			channel = 255;
+		} else if(channel > 255){
+			channel = 1;
+		}
+		
+		this.channel = channel;
+	}
+	
+	public void volume(boolean up) {
+		volume(volume + (up ? 1 : -1));
+	}
+
+	public void volume(int volume) {
+		if(!power) {
+			return;
+		}
+		
+		if(volume < 0) {
+			volume = 100;
+		} else if(volume > 100){
+			volume = 0;
+		}
+		
+		this.volume = volume;
+	}
 
 	public void status() {
 		System.out.println(
-				"TV[channel=" + channel + 
-				", volume=" + volume + 
-				", power=" + (power ? "ON" : "OFF") + "]");
+			"TV[power=" + (power ? "on" : "off") + ", " + 
+			"channel=" + channel + ", " +
+			"volume=" + volume + "]");
 	}
-
-
-
+	
 }
--- chapter03/src/main/java/tv/WatchTV.java
+++ chapter03/src/main/java/tv/WatchTV.java
@@ -7,25 +7,25 @@ public class WatchTV { // 오버로드 연습
 
 		tv.status();
 
-		tv.power( true );
-		tv.volume( 120 ); // -1 되면 100되게
+		tv.power(true);
+		tv.volume(120); // -1 되면 100되게
 		tv.status();
 
-		tv.volume( false ); // 100
+		tv.volume(false); // 100
 		tv.status();
 
 		// 라운딩
-		tv.channel( 0 );
+		tv.channel(0);
 		tv.status();
 
 		// 라운딩 오바라이딩 (코드 중복이면 고쳐야한다)
-		tv.channel( true );
-		tv.channel( true );
-		tv.channel( true );
+		tv.channel(true);
+		tv.channel(true);
+		tv.channel(true);
 
 		tv.status();
 
-		tv.power( false );
+		tv.power(false);
 
 		tv.status();
 
